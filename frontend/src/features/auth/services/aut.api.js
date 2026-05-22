c7a52c81a63f09636f6c62581ac65432fa465260
24shubhanshu <shubhanshumaurya0240@gmail.com>
2026-05-23 01:06:22 +0530

    Fixed auth api production URL

--- frontend/src/features/auth/services/aut.api.js
+++ frontend/src/features/auth/services/aut.api.js
@@ -1,69 +1,74 @@
 import axios from "axios";
 
+const api = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  withCredentials: true,
+});
 
-
- export async function register({userName,email,password}){
-
-try{
-   
-const response = await axios.post("http://localhost:3000/api/auth/register",
-    {userName,email,password},
-    {withCredentials:true});
-
-    return response.data;
-}
-catch(err)
-{
-    console.log(err);
-}
-
-}
-
-export async function login({email,password}){
-
-try{
-   
-const response = await axios.post("http://localhost:3000/api/auth/login",
-    {email,password},
-    {withCredentials:true});
+export async function register({
+  userName,
+  email,
+  password,
+}) {
+  try {
+    const response =
+      await api.post(
+        "/api/auth/register",
+        {
+          userName,
+          email,
+          password,
+        }
+      );
 
     return response.data;
-}
-catch(err)
-{
+  } catch (err) {
     console.log(err);
+  }
 }
 
-}
-
-export async function logout(){
-
-try{
-   
-const response = await axios.get("http://localhost:3000/api/auth/logout",  
-    {withCredentials:true});
+export async function login({
+  email,
+  password,
+}) {
+  try {
+    const response =
+      await api.post(
+        "/api/auth/login",
+        {
+          email,
+          password,
+        }
+      );
 
     return response.data;
-}
-catch(err)
-{
+  } catch (err) {
     console.log(err);
+  }
 }
 
-}
-
-export async function getme(){
-
-try{
-   
-const response = await axios.get("http://localhost:3000/api/auth/get-me",  
-    {withCredentials:true});
+export async function logout() {
+  try {
+    const response =
+      await api.get(
+        "/api/auth/logout"
+      );
 
     return response.data;
-}
-catch(err)
-{
+  } catch (err) {
     console.log(err);
+  }
 }
 
+export async function getme() {
+  try {
+    const response =
+      await api.get(
+        "/api/auth/get-me"
+      );
+
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
 }
